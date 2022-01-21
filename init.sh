9c86baf548e7570dd8e1c9bea4e148aa4ddcc0be
Greg Merritt <gremerritt@gmail.com>
2022-01-21 16:06:04 -0500

    Install brew, tmux, ag plus other fixes (#3)

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-export DOTFILES=$(dirname "$(readlink "$0")")
+pushd $(dirname "$(readlink "$0")") 1>/dev/null
+export DOTFILES=$(pwd)
+popd 1>/dev/null
 echo "Dotfiles are at $DOTFILES"
 
 export ZSHDIR=$HOME/.oh-my-zsh
@@ -23,8 +25,33 @@ echo "Creating ZSH theme symlink"
 rm $ZSH/themes/custom.zsh-theme 2> /dev/null
 ln -s $DOTFILES/custom.zsh-theme $ZSH/themes/custom.zsh-theme
 
+echo "Creating tmux symlink"
+rm $HOME/.tmux.conf 2>/dev/null
+ln -s $DOTFILES/tmux $HOME/.tmux.conf
+
 if [ ! -d "$FZFDIR" ]; then
     echo "Fetching and installing FZF"
     git clone --depth 1 https://github.com/junegunn/fzf.git $FZFDIR
     $FZFDIR/install
+fi
+
+if [ ! -f "~/.vim/autoload/pathogen.vim" ]; then
+  echo "Installing pathogen for vim"
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+if ! command -v brew &>/dev/null; then
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! command -v tmux &>/dev/null; then
+  echo "Installing tmux"
+  brew install tmux
+fi
+
+if ! command -v ag &>/dev/null; then
+  echo "Installing ag"
+  brew install ag
 fi
